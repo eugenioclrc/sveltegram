c950b3a3dbf5a81fd3562bf1a57c796d6a091c62
eugenioclrc <eugenioclrc@gmail.com>
2020-11-23 02:11:03 -0300

    static

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	// By default, `npm run build` will create a standard Node app.
 	// You can create optimized builds for different platforms by
 	// specifying a different adapter
-	adapter: '@sveltejs/adapter-node'
-	// adapter: '@sveltejs/adapter-static'
+	// adapter: '@sveltejs/adapter-node'
+	adapter: '@sveltejs/adapter-static'
 };
